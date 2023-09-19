bd5067733aa52624c211abe32766dafd9e39655c
tomondre <tomas.ondrejka2000@gmail.com>
2023-09-19 13:45:55 +0200

    Implementing grid creation

--- Assignment02/oo/src/board.ts
+++ Assignment02/oo/src/board.ts
@@ -15,10 +15,36 @@ export type BoardEvent<T> = ?;
 export type BoardListener<T> = ?;
 
 export class Board<T> {
+    private generator: Generator<T>;
+    public width: number;
+    public height: number;
+    public grid: T[][];
+    constructor(generator: Generator<T>, width: number, height: number) {
+        this.generator = generator;
+        this.height = height;
+        this.width = width;
+        this.grid = [];
+        this.generateGrid();
+    }
+
+    private generateGrid() {
+        for (let h = 0; h < this.height; h++) {
+            let row: T[] = [];
+            for (let w = 0; w < this.width; w++) {
+                row.push(this.generator.next());
+            }
+            this.grid.push(row);
+        }
+    }
+
     addListener(listener: BoardListener<T>) {
     }
 
     piece(p: Position): T | undefined {
+        const row = this.grid[p.row]
+        if (!row)
+            return undefined;
+        return row[p.col];
     }
 
     canMove(first: Position, second: Position): boolean {
