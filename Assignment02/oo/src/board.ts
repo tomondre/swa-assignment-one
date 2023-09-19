584be9fc94c1a88c2e9a40e4aa2acc349c61b114
tomondre <tomas.ondrejka2000@gmail.com>
2023-09-19 16:30:05 +0200

    Implementing more stuff

--- Assignment02/oo/src/board.ts
+++ Assignment02/oo/src/board.ts
@@ -1,4 +1,4 @@
-export type Generator<T>= { next:() => T } 
+export type Generator<T>= { next:() => T }
 
 export type Position = {
     row: number,
@@ -19,6 +19,7 @@ export class Board<T> {
     public width: number;
     public height: number;
     public grid: T[][];
+    private listeners: EventListener[];
     constructor(generator: Generator<T>, width: number, height: number) {
         this.generator = generator;
         this.height = height;
@@ -38,18 +39,89 @@ export class Board<T> {
     }
 
     addListener(listener: BoardListener<T>) {
+        this.listeners.push(listener);
     }
 
     piece(p: Position): T | undefined {
-        const row = this.grid[p.row]
-        if (!row)
+        if (this.isPositionOutsideOfGrid(p))
             return undefined;
-        return row[p.col];
+        return this.grid[p.row][p.col];
     }
 
     canMove(first: Position, second: Position): boolean {
+        if (this.isPositionOutsideOfGrid(first) || this.isPositionOutsideOfGrid(second))
+            return false
+        if (!(this.areSameRow(first, second) || this.areSameColumn(first, second)))
+            return false
+        return this.isMatch(first, second);
     }
-    
+
+    private isPositionOutsideOfGrid(p: Position) {
+        return (p.col < 0 || p.col >= this.width) || (p.row < 0 || p.row >= this.height);
+    }
+
+    private isMatch(first: Position, second: Position): boolean {
+        const switchedGrid = this.copyGrid();
+        const firstPiece = switchedGrid[first.row][first.col];
+        const secondPiece = switchedGrid[second.row][second.col];
+
+        switchedGrid[first.row][first.col] = secondPiece;
+        switchedGrid[second.row][second.col] = firstPiece;
+
+        // In row
+        for (let h = 0; h < this.height; h++) {
+            let rowMatchesCount = 0;
+            for (let w = 0; w < this.width - 1; w++) {
+                if (switchedGrid[h][w] === switchedGrid[h][w + 1]) {
+                    rowMatchesCount++;
+                    if (rowMatchesCount === 2)
+                        return true;
+                } else {
+                    // Makes sure that the only one type of value is matched
+                    // e.g. does not matches (1, 1, 2, 2), but matches only (1, 1, 1, 2)
+                    rowMatchesCount = 0;
+                }
+            }
+        }
+
+        // In column
+        for (let w = 0; w < this.width; w++) {
+            let columnMatchesCount = 0;
+            for (let h = 0; h < this.height - 1; h++) {
+                if (switchedGrid[h][w] === switchedGrid[h + 1][w]) {
+                    columnMatchesCount++;
+                    if (columnMatchesCount === 2)
+                        return true;
+                } else {
+                    columnMatchesCount = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    private copyGrid() {
+        const newGrid = [];
+        for (let h = 0; h < this.height; h++) {
+            let row: T[] = [];
+            for (let w = 0; w < this.width; w++) {
+                row.push(this.grid[h][w]);
+            }
+            newGrid.push(row);
+        }
+        return newGrid;
+    }
+
+    private areSameRow(first: Position, second: Position) {
+        return first.row === second.row;
+    }
+
+    private areSameColumn(first: Position, second: Position) {
+        return first.col === second.col;
+    }
+
     move(first: Position, second: Position) {
+
+        // Trigger events
     }
 }
