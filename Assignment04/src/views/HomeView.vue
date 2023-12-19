3949d4149c72a3ec9fcee389394a2e8fc610f406
JakubPlatzek <72247219+JakubPlatzek@users.noreply.github.com>
2023-12-19 19:00:55 +0100

    Login page

--- Assignment04/src/views/HomeView.vue
+++ Assignment04/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import UserAuth from '../components/UserAuth.vue'
+import Login from '../components/Login.vue'
 </script>
 
 <template>
   <main>
     <UserAuth />
+    <Login />
   </main>
 </template>
