af87cb14f0ff3ce2198d8e86bb6f4e5eced72e64
Constantin Ginga <41089075+constantinginga@users.noreply.github.com>
2023-12-19 18:31:07 +0100

    Add Vue project

---
+++ Assignment04/src/components/UserAuth.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const username = ''
+</script>
+
+<template>
+  <input type="text" v-model="username" />
+</template>
---
+++ Assignment04/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import UserAuth from '../components/UserAuth.vue'
+</script>
+
+<template>
+  <main>
+    <UserAuth />
+  </main>
+</template>
